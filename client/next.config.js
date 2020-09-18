2ccf66941416afa74fe0525e09229110ecc7a061
Jack <jbizzell@protonmail.com>
2020-09-18 23:34:11 +0100

    comment explaining what this is for

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,7 @@
+//for better automatic file change watching in kubernetes/skaffold
 module.exports = {
-  webpackDevMiddleware: config => {
-    config.watchOptions.poll = 300
+  webpackDevMiddleware: (config) => {
+    config.watchOptions.poll = 300;
     return config;
-  }
-}
+  },
+};
